0f95ab612419a7aa344e8d5864675fcd105328c2
Daniel Pustotin <heartsker@gmail.com>
2022-03-28 19:27:07 +0300

    Initialization of the Broken
    Project created

---
+++ Broken/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Broken
+//
+//  Created by Daniel Pustotin on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
