6ca04f3d76610da77a5ee0e70034339a31c2ba91
Ana-rideto <97951307+Ana-rideto@users.noreply.github.com>
2022-04-07 20:04:16 +0300

    [TIC 14]: Buttons added to Level (#14)

---
+++ Broken/Model/ButtonsSet.swift
@@ -0,0 +1,27 @@
+//
+//  ButtonsSet.swift
+//  Broken
+//
+//  Created by Mac on 06.04.2022.
+//
+
+import Foundation
+
+class ButtonsSet {
+    var buttons: [Button: Bool]
+    init(all: Bool = true) {
+        buttons = [:]
+        for buttonCase in Button.allCases {
+            buttons[buttonCase] = all
+        }
+    }
+    init(activeButtons: [Button]) {
+        buttons = [:]
+        for button in Button.allCases {
+            buttons[button] = false
+        }
+        for button in activeButtons {
+            buttons[button] = true
+        }
+    }
+}
