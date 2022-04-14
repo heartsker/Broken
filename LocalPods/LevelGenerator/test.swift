5aee2470536cc24facf966d97cbf46c92a3e480c
Daniel Pustotin <heartsker@gmail.com>
2022-04-14 19:27:32 +0300

    Test

--- LocalPods/LevelGenerator/test.swift
+++ LocalPods/LevelGenerator/test.swift
@@ -1,12 +1,12 @@
 //
-//  LevelGenerator.swift
-//  LevelGenerator
+//  test.swift
+//  Pods
 //
 //  Created by Daniel Pustotin on 14.04.2022.
 //
 
 import Foundation
 
-class Levelgenerator {
-
+class Test {
+    
 }
