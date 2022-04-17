ec9d487d9446eb4ad48cd52e28ab5057268682be
Daniel Pustotin <heartsker@gmail.com>
2022-04-17 13:07:31 +0300

    Improved python LevelGenerator

--- LevelGenerator/generator.py
+++ LevelGenerator/generator.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import random
+from random import random, randint, choice, choices, sample
 
 buttonType = {
     'ZERO' : '0',
@@ -21,41 +21,48 @@ buttonType = {
     'OPEN': '(',
     'CLOSE': ')'
 }
-    
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+operators = ['+', '-', '*', '/', '*(-1)', '.']
+# brackets = ['(', ')']
 
 class Level:
 
-    def __init__(self, start, finish, buttons):
+    def __init__(self, start, finish, numbers, operators):
         self.start = start
         self.finish = finish
-        self.buttons = buttons
+        self.numbers = numbers
+        self.operators = operators
+        # self.brackets = brackets
 
     def print(self):
         print('Level:')
         print('\tStart:', self.start)
         print('\tFinish:', self.finish)
-        print('\tButtons:', self.buttons)
+        print('\tNumber:', self.numbers)
+        print('\tOperators:', self.operators)
+        # print('\tBrackets:', self.brackets)
 
 def generate():
-    start = random.randint(-100, 100)
-    finish = random.randint(-100, 100)
-    count = random.randint(2, len(buttonType) )
-    buttons = random.sample(list(buttonType.keys()), count)
+    start = randint(-100, 100)
+    finish = randint(-100, 100)
+    count = randint(1, len(numbers))
+    num = sample(list(numbers), count)
+
+    count = randint(0, len(operators))
+    op = sample(list(operators), count)
 
     # print(start, finish, count, buttons)
-    return Level(start, finish, buttons)
-    
+    return Level(start, finish, num, op)
 
 # level = generate()
 # level.print()
-
-# level = Level(1, 2, ['PLUS', 'ONE'])
 
 def isSign(char):
     return ['+', '-', '*', '/'].count(char)
 
 def check(level):
-    iterations = 100000
+    iterations = 10000
     max_clicks = 15
 
     success = 0
@@ -63,16 +70,35 @@ def check(level):
 
     for i in range(iterations):
         line = str(level.start)
+        hasPoint = False
+        br_count = 0
         for j in range(max_clicks):
-            button = random.choice(level.buttons)
 
-            # print(line)
-            if len(line) and isSign(line[-1]) and isSign(buttonType[button]):
-                line = line[:-1]
+            p = random()
 
-            # print(line)
-            
-            line += buttonType[button]
+            if p < 0.5 and len(level.operators):
+                # add operator
+                if isSign(line[-1]):
+                    line = line[:-1]
+                
+                line += choice(level.operators)
+                hasPoint = False
+            # elif p < 0.5 and level.brackets and isSign(line[-1]):
+            #     # add bracket (
+            #     line += '('
+            #     br_count += 1
+            #     hasPoint = False
+            # elif p < 0.6 and level.brackets and br_count > 0:
+            #     line += ')'
+            #     br_count -= 1
+            #     hasPoint = False
+            else:
+                # add number or point
+                if p < 0.6 and level.operators.count('.') and not hasPoint:
+                    line += '.'
+                    hasPoint = True
+                else:
+                    line += choice(level.numbers)
 
             # print(line)
 
@@ -82,7 +108,6 @@ def check(level):
             except:
                 result = level.finish - 1
                 continue
-
 
             count += 1
             if result == level.finish:
@@ -99,18 +124,39 @@ def check(level):
 
 levels = 0
 count = 0
+eps = 0.0001
+
+ready = []
 
 while True:
     level = generate()
     result = check(level)
 
-    if result[0]:
+    percent = result[0] / result[1]
+
+    if percent > eps:
         levels += 1
-        print('Level #', levels, sep='')
-        level.print()
-        print(result[0], 'out of', result[1], '-', result[0] / result[1])
-        print('--------------------------------')
+        # print('Level #', levels, sep='')
+        # level.print()
+        # print(result[0], 'out of', result[1], '-', percent)
+        # print('--------------------------------')
+        ready.append((percent, level))
 
     count += 1
 
-    print('--------', levels, 'out of', count, '-----------')
+    # print('--------', levels, 'out of', count, '-----------')
+
+    if levels == 50:
+        break
+
+ready.sort()
+ready.reverse()
+
+for i in range(len(ready)):
+    (percent, level) = ready[i]
+
+    print('Level #', i + 1, sep='')
+
+    print('\tPercent:', percent)
+
+    level.print()
