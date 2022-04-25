84ee47988946520c660e39a6c005db03694d487d
krisMesh <icetimi@gmail.com>
2022-04-25 20:01:11 +0300

    button added, coordinator updated. Second try

--- Broken/HomeView.swift
+++ Broken/HomeView.swift
@@ -29,14 +29,3 @@ class HomeViewController: UIViewController {
     }
 
 }
-// struct HomeView: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-// }
-//
-// struct HomeView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        HomeView()
-//    }
-// }
