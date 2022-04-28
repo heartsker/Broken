9fbd32d7c8ad192969ce25b444c65725f6fd86f6
Daniel Pustotin <51203539+heartsker@users.noreply.github.com>
2022-04-28 17:56:27 +0300

    Tic 13 (#24)
    * removed storyboard
    
    * Fixed launch
    
    * button added, coordinator updated
    
    * button added, coordinator updated. Second try
    
    Co-authored-by: krisMesh <icetimi@gmail.com>

--- Broken/LevelsView.swift
+++ Broken/LevelsView.swift
@@ -15,15 +15,3 @@ class LevelsViewController: UIViewController {
         title = "LevelsView"
     }
 }
-
-// struct LevelsView: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-// }
-//
-// struct LevelsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        LevelsView()
-//    }
-// }
