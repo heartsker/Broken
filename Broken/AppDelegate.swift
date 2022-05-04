2539d7f6c40800fce561e19d846f42ea175d0b6e
Матвей Кузнецов <matveikuznetsov@MacBook-Pro-Matvej.local>
2022-05-04 17:55:34 +0300

    Added Level Storage

--- Broken/AppDelegate.swift
+++ Broken/AppDelegate.swift
@@ -5,11 +5,33 @@
 //  Created by Daniel Pustotin on 28.03.2022.
 //
 
-import UIKit
-
+ import UIKit
+import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+// MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        let container = NSPersistentCloudKitContainer(name: "Broken")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
@@ -27,3 +49,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+
---
+++ Broken/CoreData/CoreDataError.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataError.swift
+//  Broken
+//
+//  Created by Матвей Кузнецов on 02.05.2022.
+//
+
+import Foundation
+enum CoreDataError: LocalizedError {
+    case fetchingError(NSManagedObject.Type)
+
+    var errorDescription: String? {
+        switch self {
+        case let .fetchingError(type):
+            return "Failed to fetch entity of type \(type)"
+        }
+    }
+}
